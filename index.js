f491d7c9e939ee6ffc96d871ca53b5e9dac82632
8AAssemblyGameShow <84619541+8AAssemblyGameShow@users.noreply.github.com>
2021-05-26 12:58:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ function updateDB(){
     var db = firebase.firestore();
     userid = document.getElementById("userName").value;
     std = document.getElementById("std").value;
-    classId = document.getElementById("email").value;
     if(userid == "" || std == "" || classId == ""){
     alert("Some fields are empty")}else{
 
     db.collection("users").add({
         name: userid,
         std: std,
-        email: classId,
         score: 0
     })
     .then((docRef) => {
